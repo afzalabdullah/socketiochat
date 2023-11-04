2d57b8a9f096db93a671c1a9fa4fae53b45898ac
Abdullah Afzal <98462344+afzalabdullah@users.noreply.github.com>
2023-11-04 23:07:46 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,61 @@
-const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
-const socketio = require('socket.io')(http);
+const WebSocket = require('ws')
+const express = require('express')
+const moment = require('moment')
+const app = express()
+const port = 7878; //port for https
 
 app.get('/', (req, res) => {
-    res.send("Server is running bro");
+    res.send("Server is Running");
 });
 
-socketio.on("connection", (userSocket) => {
-    userSocket.on("send_message", (data) => {
-        userSocket.broadcast.emit("receive_message", data);
-    });
-
-    userSocket.on("typing", (data) => {
-        userSocket.broadcast.emit("typing", data);
-    });
-
-    userSocket.on("stop_typing", (data) => {
-        userSocket.broadcast.emit("stop_typing", data);
-    });
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
 });
+var  webSockets = {}
 
-http.listen(process.env.PORT || 3000, () => {
-    console.log("Server is running on port 8080");
-});
+const wss = new WebSocket.Server({ port: 6060 }) //run websocket server with port 6060
+wss.on('connection', function (ws, req)  {
+    var userID = req.url.substr(1) //get userid from URL ip:6060/userid 
+    webSockets[userID] = ws //add new user to the connection list
 
+    console.log('User ' + userID + ' Connected ') 
+
+    ws.on('message', message => { //if there is any message
+        console.log(message);
+        var datastring = message.toString();
+        if(datastring.charAt(0) == "{"){
+            datastring = datastring.replace(/\'/g, '"');
+            var data = JSON.parse(datastring)
+            if(data.auth == "EvenySynergy"){
+                if(data.cmd == 'send'){ 
+                    var boardws = webSockets[data.userid] //check if there is reciever connection
+                    if (boardws){
+                        var cdata = "{'cmd':'" + data.cmd + "','userid':'"+data.userid+"', 'msgtext':'"+data.msgtext+"'}";
+                        boardws.send(cdata); //send message to reciever
+                        ws.send(data.cmd + ":success");
+                    }else{
+                        console.log("No reciever user found.");
+                        ws.send(data.cmd + ":error");
+                    }
+                }else{
+                    console.log("No send command");
+                    ws.send(data.cmd + ":error");
+                }
+            }else{
+                console.log("App Authincation error");
+                ws.send(data.cmd + ":error");
+            }
+        }else{
+            console.log("Non JSON type data");
+            ws.send(data.cmd + ":error");
+        }
+    })
+
+    ws.on('close', function () {
+        var userID = req.url.substr(1)
+        delete webSockets[userID] //on connection close, remove reciver from connection list
+        console.log('User Disconnected: ' + userID)
+    })
+    
+    ws.send('connected'); //innitial connection return message
+})
